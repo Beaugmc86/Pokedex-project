d8052067c3d1570bd308200a54f88a8086f596b6
Beaugmc86 <153542400+Beaugmc86@users.noreply.github.com>
2024-01-11 18:24:19 -0800

    Added for loop, background color, and favicon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
+//Create variable for pokemon array.
 let pokemonList = [
-  { name: 'Bulbasaur', type: ['grass', 'poison'], height: 7 },
-  { name: 'Charmander', type: 'fire', height: 6 },
-  { name: 'Squirtle', type: 'water', height: 5},
+  { 
+    name: 'Bulbasaur', 
+    type: ['grass', 'poison'], 
+    height: 0.7 
+  },
+  { 
+    name: 'Charmander', 
+    type: 'fire', 
+    height: 0.6 
+  },
+  { 
+    name: 'Squirtle', 
+    type: 'water', 
+    height: 0.5
+  },
 ];
+
+//Loop to iterate over each item in pokemonList
+for (let i = 0; i < pokemonList.length; i++){
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' )');
+  if (pokemonList[i].height < 0.7){
+   document.write('<br>')
+  }else {
+    document.write(' - Wow, that\'s big!');
+    document.write('<br>')
+  }
+}
