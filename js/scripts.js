2c2e168b225c59c9288d4f0c7b81f51806615666
Beaugmc86 <153542400+Beaugmc86@users.noreply.github.com>
2024-01-16 16:12:10 -0800

    Task 1.5 pt 2 - Wrap pokemonList array in IIFE.
    Wrapped list in IIFE and updated old 'for' loop incorporate pokemonRepository.

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,58 @@
 //Create variable for pokemon repository.
-let pokemonList = [
-  { 
-    name: 'Bulbasaur', 
-    type: ['grass', 'poison'], 
-    height: 0.7 
-  },
-  { 
-    name: 'Charmander', 
-    type: 'fire', 
-    height: 0.6 
-  },
-  { 
-    name: 'Squirtle', 
-    type: 'water', 
-    height: 0.5
-  },
-  { 
-    name: 'Mr. Mime', 
-    type: ['psychic', 'fairy'], 
-    height: 1.3 
-  },
-  { 
-    name: 'Jynx', 
-    type: ['psychic', 'ice'], 
-    height: 1.4 
-  },
-  { 
-    name: 'Snorlax', 
-    type: 'normal', 
-    height: 2.1
-  },
-];
+// Wrap list array in an IIFE to avoid accidentally accessing global state
+let pokemonRepository = (function () {
 
-//Loop to iterate over each item in pokemonList
-// NOTE this is the old code to be replaced in Task 1.5
-// function printArrayDetails(list){
-//   for (let i = 0; i < list.length; i++){
-//     document.write('<p>' + list[i].name + ' (height: ' + list[i].height + ' )');
-//     if (list[i].height < 1.0){
-//     }else {
-//       document.write(' - Wow, that\'s big!');
-//     }
-//   }
-// }
+  let pokemonList = [
+    { 
+      name: 'Bulbasaur', 
+      type: ['grass', 'poison'], 
+      height: 0.7 
+    },
+    { 
+      name: 'Charmander', 
+      type: 'fire', 
+      height: 0.6 
+    },
+    { 
+      name: 'Squirtle', 
+      type: 'water', 
+      height: 0.5
+    },
+    { 
+      name: 'Mr. Mime', 
+      type: ['psychic', 'fairy'], 
+      height: 1.3 
+    },
+    { 
+      name: 'Jynx', 
+      type: ['psychic', 'ice'], 
+      height: 1.4 
+    },
+    { 
+      name: 'Snorlax', 
+      type: 'normal', 
+      height: 2.1
+    },
+  ]
 
-// printArrayDetails(pokemonList); // executes the function using ‘pokemonList‘ as its input
+  //Return array of pokemon
+  function getAll () {
+    return pokemonList;
+  }
+  //Add a pokemon item to a list
+  function add (pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-// printArrayDetails(pokemonList2); // executes the function using ‘pokemonList2‘ as its input
+  return {
+    getAll: getAll,
+    add: add
+  }
+})()
 
 //forEach Loop to iterate over pokemonList in task 1.5
-pokemonList.forEach(function(pokemon) {
+//Updated from 'pokemonList' to 'pokemonRepository.getAll()' to call function withing IIFE/
+pokemonRepository.getAll().forEach(function(pokemon) {
   document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ' )' )
-  if (pokemon.height > 1.0){
-    document.write(' - Wow, that\'s big!');
-    }
   }
 )
